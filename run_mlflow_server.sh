ee25c3866b22594a368497373d9e9329710788e1
alexnikzotov <miccormicco-risen@yandex.ru>
2025-07-14 10:08:21 +0000

    Удачный запуск MLFlow

--- run_mlflow_server.sh
+++ run_mlflow_server.sh
@@ -1,9 +1,9 @@
 export MLFLOW_S3_ENDPOINT_URL=https://storage.yandexcloud.net
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-export AWS_BUCKET_NAME=$S3_BUCKET_NAME
+export AWS_ACCESS_KEY_ID='YCAJE3Nlz8iDILW5VTYM1ihQB'
+export AWS_SECRET_ACCESS_KEY='YCPjvS7uwhvJpUj3bKm8X-IX4QAwBIVsvX61IL44'
+export AWS_BUCKET_NAME='s3-student-mle-20250529-05fed48463'
 
 mlflow server \
-  --backend-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME\
-    --default-artifact-root s3://$AWS_BUCKET_NAME \
+  --backend-store-uri postgresql://mle_20250529_05fed48463:0c567edd8ad8472e87d5c85cc4d664e4@rc1b-uh7kdmcx67eomesf.mdb.yandexcloud.net:6432/playground_mle_20250529_05fed48463\
+    --default-artifact-root s3://s3-student-mle-20250529-05fed48463 \
     --no-serve-artifacts 
